65ee4e65e6ba54d2a56b935bd4e9a267e3d9238a
AbbeVass <134689455+AbbeVass@users.noreply.github.com>
2026-06-17 17:14:31 +0200

    Add Mantine and remove Vite template

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import "@mantine/core/styles.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
 const redirect = sessionStorage.redirect;
