6168ad99441b93ae01837c1b15dd15da2dd59fd6
AbbeVass <134689455+AbbeVass@users.noreply.github.com>
2026-06-17 17:38:58 +0200

    Move MantineProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { MantineProvider } from '@mantine/core'
 import NotFound from './pages/NotFound.tsx'
 import Home from './pages/Home.tsx'
 
 function App() {
   return (
-    <MantineProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="tone-ladder/*" element={<NotFound />} />
-          <Route path="tone-ladder/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </MantineProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="tone-ladder/*" element={<NotFound />} />
+        <Route path="tone-ladder/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
-import "@mantine/core/styles.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 import App from './App.tsx'
 
 const redirect = sessionStorage.redirect;
@@ -12,6 +13,8 @@ if (redirect) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
