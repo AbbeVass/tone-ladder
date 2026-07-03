f124422e2d1bd622697238ccf1c7f49db1277d8b
AbbeVass <134689455+AbbeVass@users.noreply.github.com>
2026-07-03 18:42:30 +0200

    Update 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom"
+import { Flex, Title, Text } from "@mantine/core"
+import { TEXT_COLOR } from "../defs/constants"
 
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <Link to="/tone-ladder/">Go back to Home</Link>
-    </div>
+    <Flex
+      direction="column"
+      align="center"
+      justify="center"
+      gap={20}
+      m={50}
+    >
+      <Title c={TEXT_COLOR}>404 - Not Found</Title>
+      <Link to="/tone-ladder/">
+        <Text
+          size="xl"
+          c={TEXT_COLOR}
+          td="underline"
+        >
+          Go back to Home
+        </Text>
+      </Link>
+    </Flex>
   )
 }
 
